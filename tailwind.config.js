c8357c3daad488d5d818689f8bf660cdf4ebd248
suryansH tomar <you@example.com>
2022-07-07 15:32:07 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        'bi': "url('bimg.jpg')",
+      },
+    },
+  },
+  plugins: [],
+}
